a6f650df65ef2e5148341d510b5a1cf3deef0d24
really650a <99811385+really650a@users.noreply.github.com>
2023-07-30 14:53:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ Telegram.WebApp.ready();
 btn.addEventListener("click", () => {
   
   const data = Telegram.WebApp.initDataUnsafe;
-  Telegram.WebApp.sendData(data);
+  Telegram.WebApp.sendData("Huh");
   Telegram.WebApp.close();;
   const tag = document.createElement("p");
   
