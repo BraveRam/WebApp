9f965787fb54f3ca7e4ebb984e624dc8c5a90924
really650a <99811385+really650a@users.noreply.github.com>
2023-07-30 10:38:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-const btn = document.querySelector("button")
-const main = document.querySelector(".main")
+const btn = document.querySelector("button");
+const main = document.querySelector(".main");
 
 const tele = window.Telegram.WebApp;
 
-btn.addEventListener("click", ()=>{
-  tele.ready()
+btn.addEventListener("click", () => {
+  tele.ready();
   const data = tele.initDataUnsafe;
-  const tag = document.createElement("p")
-  tag.innerHTML = data;
-  main.appendChild(tag)
-})
+  const tag = document.createElement("p");
+  
+  // Retrieve user ID and username
+  const userId = data.user.id;
+  const username = data.user.username;
+  
+  // Display user ID and username
+  tag.innerHTML = `User ID: ${userId}<br>Username: ${username}`;
+  main.appendChild(tag);
+});
