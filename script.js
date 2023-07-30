07d798ac47626684e1e36113df86385167814ad8
really650a <99811385+really650a@users.noreply.github.com>
2023-07-30 16:43:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ btn.addEventListener("click", () => {
   //const userId = data.user.id;
   //const username = data.user.username;
   Telegram.WebApp.sendData("Hello");
-  Telegram.WebApp.close();
+  //Telegram.WebApp.close();
   const tag = document.createElement("p");
   tag.innerHTML = data.user.username;
   main.appendChild(tag);
