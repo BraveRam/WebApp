a2b890bdd58454311aca435797119e1b1953c9b3
really650a <99811385+really650a@users.noreply.github.com>
2023-07-30 14:47:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,24 +3,25 @@ const main = document.querySelector(".main");
 
 Telegram.WebApp.ready();
 
-Telegram.WebApp.MainButton.setText('Send Data').show().onClick(function () {
+/*Telegram.WebApp.MainButton.setText('Send Data').show().onClick(function () {
   const data = "Hello"
     Telegram.WebApp.sendData(data);
     Telegram.WebApp.close();
-    });
+    });*/
 
 
-/*btn.addEventListener("click", () => {
-  tele.ready();
-  const data = tele.initDataUnsafe;
+btn.addEventListener("click", () => {
+  const data = "Hello"
+  Telegram.WebApp.sendData(data);
+  Telegram.WebApp.close();;
+  /*const data = tele.initDataUnsafe;
   const tag = document.createElement("p");
   
-  // Retrieve user ID and username
   const userId = data.user.id;
   const username = data.user.username;
   
   // Display user ID and username
   tag.innerHTML = `User ID: ${userId}<br>Username: ${username}`;
-  main.appendChild(tag);
+  main.appendChild(tag);*/
   
-});*/
+});
