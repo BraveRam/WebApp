eba06130677d0b8501bbf44e726c16175235f318
really650a <99811385+really650a@users.noreply.github.com>
2023-07-30 18:30:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,12 +12,22 @@ Telegram.WebApp.ready();
 
 btn.addEventListener("click", () => { 
   const data = Telegram.WebApp.initDataUnsafe;
-  //const userId = data.user.id;
+  const userId = data.user.id;
+  
+  const botToken = "6464751970:AAFfVZYceyuZV3gExwPELbnCwvKZtx1wdJw";
+const chatId = userId;
+const messageText = "Hello, world!";
+
+fetch(`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${messageText}`)
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.log(error));
+
   //const username = data.user.username;
-  Telegram.WebApp.sendData("Hello");
+  //Telegram.WebApp.sendData("Hello");
   //Telegram.WebApp.close();
-  const tag = document.createElement("p");
-  tag.innerHTML = data.user.username;
-  main.appendChild(tag);
+  //const tag = document.createElement("p");
+  //tag.innerHTML = data.user.username;
+  //main.appendChild(tag);
   
 });
